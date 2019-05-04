517fb2d4eda2a214db56ef412d4b44f5ba37ac31
Ovidiu Cara <ovidiu92@gmail.com>
2019-05-04 17:53:16 +0200

    Implemented Recycler View

--- app/src/main/java/com/example/taskscheduler/MainActivity.java
+++ app/src/main/java/com/example/taskscheduler/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.taskscheduler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private ArrayList<String> mNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started.");
+        initListItems();
     }
+
+
+    private void initListItems(){
+        Log.d(TAG, "initListItems: preparing bitmaps.");
+        mNames.add("Havasu Falls");
+        mNames.add("Trondheim");
+        mNames.add("Portugal");
+        mNames.add("Rocky Mountain National Park");
+        mNames.add("Mahahual");
+        mNames.add("Frozen Lake");
+        initRecyclerView();
+    }
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview.");
+        RecyclerView recyclerView = findViewById(R.id.my_recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
 }
---
+++ app/src/main/java/com/example/taskscheduler/RecyclerViewAdapter.java
@@ -0,0 +1,63 @@
+package com.example.taskscheduler;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mItemNames = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<String> mItemNames) {
+        this.mItemNames = mItemNames;
+        this.mContext = mContext;
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+        viewHolder.listItem.setText(mItemNames.get(position));
+
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Log.d(TAG, "onClick: clicked on" + mItemNames.get(position));
+                Toast.makeText(mContext, mItemNames.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItemNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView listItem;
+        RelativeLayout parentLayout;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            listItem = itemView.findViewById(R.id.list_item);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
