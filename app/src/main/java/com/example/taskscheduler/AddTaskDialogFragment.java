c0903d64443e837f583bea17f42cdf2153f88c2d
Ovidiu Cara <ovidiu92@gmail.com>
2019-06-01 21:50:12 +0200

    Added FAB and Dialog View

---
+++ app/src/main/java/com/example/taskscheduler/AddTaskDialogFragment.java
@@ -0,0 +1,64 @@
+package com.example.taskscheduler;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+
+public class AddTaskDialogFragment extends DialogFragment {
+
+    /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface NoticeDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+    // Use this instance of the interface to deliver action events
+    NoticeDialogListener listener;
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            listener = (NoticeDialogListener) context;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException();
+        }
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_add_task, null))
+                // Add action buttons
+                .setPositiveButton(R.string.add_task, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        return builder.create();
+    }
+
+
+
+}
--- app/src/main/java/com/example/taskscheduler/MainActivity.java
+++ app/src/main/java/com/example/taskscheduler/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.taskscheduler;
 
+import android.app.Dialog;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,7 +16,7 @@ import android.widget.EditText;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends FragmentActivity implements AddTaskDialogFragment.NoticeDialogListener{
     private static final String TAG = "MainActivity";
 
     private ArrayList<String> mNames = new ArrayList<>();
@@ -28,6 +33,17 @@ public class MainActivity extends AppCompatActivity{
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    showAddDialog();
+            }
+        });
+
+
+/*
         final EditText edit = (EditText) findViewById(R.id.editText);
         final Button button = (Button) findViewById(R.id.btnAddItem);
         // Register the onClick listener with the implementation above
@@ -39,7 +55,29 @@ public class MainActivity extends AppCompatActivity{
 
             }
         });
+*/
+    }
+// TODO https://developer.android.com/guide/topics/ui/dialogs#PassingEvents
+    public void showAddDialog(){
+        DialogFragment dialog = new AddTaskDialogFragment();
+        dialog.show(getSupportFragmentManager(), "task");
+    }
 
+    // The dialog fragment receives a reference to this Activity through the
+    // Fragment.onAttach() callback, which it uses to call the following methods
+    // defined by the NoticeDialogFragment.NoticeDialogListener interface
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // User touched the dialog's positive button
+        Dialog dialogView = dialog.getDialog();
+        EditText editText = (EditText) dialogView.findViewById(R.id.task_name);
+        Log.d(TAG, "onClick: I've been clicked." + " " + editText);
+
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        // User touched the dialog's negative button
     }
 
     private void initRecyclerView(){
